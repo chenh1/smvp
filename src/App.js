45b3182eea2a9538959c4d42b06eb9a0493a6cb6
Howard Chen <chenh0886@gmail.com>
2018-08-04 17:48:07 -0700

    Add pages and react-router

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
-
+import Dashboard from './pages/Dashboard';
+import SessionPage from './pages/SessionPage';
 
 // Ensure that uri matches server uri
 const client = new ApolloClient({
@@ -15,6 +17,12 @@ const App = () => (
   <ApolloProvider client={client}>
     <div>
       <h2>My first Apollo app 🚀</h2>
+
+      <Router>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/session" component={SessionPage} />
+      </Router>
 
       <LoginPage />
     </div>
---
+++ src/pages/SessionPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { compose, mapProps, withHandlers, pure } from 'recompose';
+//import { graphql } from 'react-apollo';
+//import { login } from '../tags/user';
+//import { call, get } from '../helpers/webapi';
+
+const SessionPage = (props) => {
+    console.log('props: ', props)
+    return (
+    <div>
+        SessionPage:
+    </div>
+    )
+};
+
+export default compose(
+    // Throw stuff here
+)(SessionPage);
